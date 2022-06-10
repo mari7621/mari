30db593bcbe6055f5d6a5fe8cc12149d686d3670
伊藤真理 <itoumari@itoumarinoMacBook-Air-2.local>
2022-06-10 18:40:02 +0900

    first commit

---
+++ js2/main.js
@@ -0,0 +1,22 @@
+$(function($){
+    $('.bg-swicher').bgSwitcher({
+        images:['images/bg1.jpg','images/bg2.jpg','images/bg3.jpg'],
+        interval:5000,
+        loop:true
+    });
+
+    $(window).scroll(function(){
+var targetElement=$('.fadein').offset().top;
+var scroll=$(window).scrollTop();
+var windowHeight=$(window).height();
+if(scroll>targetElement-windowHeight+200){
+    $('.fadein').css('opacity','1');
+    $('.fadein').css('transform','translateY(0)');
+
+}
+
+    });
+
+});
+
+
